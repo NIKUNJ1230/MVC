e9185c9b506bb55aa4bb0aa5275e20e1eef2ad2a
NIKUNJ NAVAPARA <133842892+NIKUNJ1230@users.noreply.github.com>
2023-07-26 09:32:03 +0530

    new

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const express = require('express');
-const connection = require('./config/database');
-const userroute = require('./routes/user')
+const express = require("express");
+const connection = require("./config/database");
+const userroute = require("./routes/user");
 require("dotenv").config();
 const app = express();
 
 let port = process.env.Port || 8090;
 app.use(express.json());
 
-app.use("/user",userroute)
-
+app.use("/user", userroute);
 
 app.listen(port, () => {
-    console.log(`listening on ${port}`);
-    connection()
-  });
+  console.log(`listening on ${port}`);
+  connection();
+});
